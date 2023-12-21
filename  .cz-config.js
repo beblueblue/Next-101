b224f135ccaada2d4b4ebf7d572387ca7473551d
HHZ <860601963@qq.com>
2023-12-21 21:40:13 +0800

    chore(crate project): format git

---  .cz-config.js
+++  .cz-config.js
@@ -1,31 +1,42 @@
 module.exports = {
-  // 可选类型
   types: [
-    { value: 'feat', name: 'feat:     新功能' },
-    { value: 'fix', name: 'fix:      修复' },
-    { value: 'docs', name: 'docs:     文档变更' },
-    { value: 'style', name: 'style:    代码格式(不影响代码运行的变动)' },
     {
-      value: 'refactor',
-      name: 'refactor: 重构(既不是增加feature，也不是修复bug)',
+      value: 'feat',
+      name: '✨ feat:     新功能',
     },
-    { value: 'perf', name: 'perf:     性能优化' },
-    { value: 'test', name: 'test:     增加测试' },
-    { value: 'chore', name: 'chore:    构建过程或辅助工具的变动' },
-    { value: 'revert', name: 'revert:   回退' },
-    { value: 'build', name: 'build:    打包' },
+    {
+      value: 'fix',
+      name: '🐛 fix:      修复bug',
+    },
+    {
+      value: 'init',
+      name: '🎉 init:     初始化',
+    },
+    {
+      value: 'docs',
+      name: '✏️  docs:     文档变更',
+    },
+    {
+      value: 'style',
+      name: '💄 style:    代码的样式美化',
+    },
+    {
+      value: 'chore',
+      name: '🚀 chore:    构建/工程依赖/工具',
+    },
   ],
-  // 消息步骤
+  scopes: [{ name: 'crate project' }, { name: 'routing' }],
   messages: {
-    type: '请选择提交类型:',
-    customScope: '请输入修改范围(可选):',
-    subject: '请简要描述提交(必填):',
-    body: '请输入详细描述(可选):',
-    footer: '请输入要关闭的issue(可选):',
-    confirmCommit: '确认使用以上信息提交？(y/n/e/h)',
+    type: '请选择提交类型(必填)',
+    customScope: '请输入文件修改范围(可选)',
+    subject: '请简要描述提交(必填)',
+    body: '请输入详细描述(可选)',
+    breaking: '列出任何BREAKING CHANGES(可选)',
+    footer: '请输入要关闭的issue(可选)',
+    confirmCommit: '确定提交此说明吗？(y/n/e/h)',
   },
-  // 跳过问题
-  skipQuestions: ['body', 'footer'],
-  // subject文字长度默认是72
+  skipQuestions: ['body', 'breaking', 'footer'],
+  allowCustomScopes: true,
+  allowBreakingChanges: [':sparkles: feat', ':bug: fix'],
   subjectLimit: 72,
 };
