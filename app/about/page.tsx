38610c78086d5ad68f9a9b99ae2950500af464ce
HHZ <860601963@qq.com>
2023-12-22 15:19:18 +0800

    feat(routing): define route

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import styles from '../page.module.css';
+
+export default function About() {
+
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <h1>The About page</h1>
+      </div>
+    </main>
+  );
+}
