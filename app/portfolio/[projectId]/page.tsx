62a8b3cdddccd56dafe8e3f056fa066cba7ef65e
HHZ <860601963@qq.com>
2023-12-29 17:48:52 +0800

    feat(routing): use navigations

--- app/portfolio/[projectId]/page.tsx
+++ app/portfolio/[projectId]/page.tsx
@@ -1,12 +1,27 @@
+'use client';
+
 import styles from '../../page.module.css';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 
 export default function PortfolioProject({
   params,
 }: {
   params: { projectId: string };
 }) {
-  // This log will be printed on the server.
+  // When we set the component as server component by default, the log will be printed only on server.
+  // When we set the component as client component by 'use client', the log will be printed on both server and client.
   console.log('params:', params);
+
+  const pathName = usePathname();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  console.log('navigation: ', {
+    pathName,
+    router,
+    searchParams,
+  });
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
