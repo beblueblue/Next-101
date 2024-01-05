a7430b7362959c606b821793504c79b860daecaa
HHZ <860601963@qq.com>
2024-01-05 16:50:34 +0800

    feat(routing): navigating with Link component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,25 @@
 import styles from './page.module.css';
+import Link from 'next/link';
 
 export default function Home() {
-
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <h1>The Home Page</h1>
       </div>
+      <ul>
+        {/* <li> */}
+        {/* It can work. */}
+        {/* But it is not recommended, it will send a new http request, and all app state would be lost */}
+        {/* <a href="/portfolio">portfolio</a> */}
+        {/* </li> */}
+        <li>
+          <Link href={'/portfolio'}>portfolio</Link>
+        </li>
+        <li>
+          <Link href={'/clients'}>clients</Link>
+        </li>
+      </ul>
     </main>
   );
 }
